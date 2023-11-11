02db6bec429e4df112b20e0c2a2926eb855e881f
Allan Leandro <alanleandrof@gmail.com>
2023-11-11 20:08:16 -0300

    documentacao

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -1,28 +1,136 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Cadastro
+ *   description: API para gerenciamento de cadastros
+ */
+
+/**
+ * @swagger
+ * /cadastro/inserir:
+ *   post:
+ *     summary: Inserir um novo cadastro
+ *     tags: [Cadastro]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nomeSobrenome:
+ *                 type: string
+ *               nomeUsuario:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Cadastro inserido com sucesso
+ *         content:
+ *           application/json:
+ *             example:
+ *               result:
+ *                 codigo: 123
+ *                 nomeSobrenome: John Doe
+ *                 nomeUsuario: johndoe
+ *                 email: johndoe@example.com
+ *                 senha: ********
+ *       400:
+ *         description: Campos não enviados
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Campos não enviados
+ */
+
 const cadastroServices = require('../services/cadastroServices');
-const cadatroService = require('../services/cadastroServices');
 
 module.exports = {
-    inserir: async(req, res) => {
-        let json = {error:'', result:{}};
+    /**
+     * @swagger
+     * /cadastro/inserir:
+     *   post:
+     *     summary: Inserir um novo cadastro
+     *     tags: [Cadastro]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               nomeSobrenome:
+     *                 type: string
+     *               nomeUsuario:
+     *                 type: string
+     *               email:
+     *                 type: string
+     *               senha:
+     *                 type: string
+     *     responses:
+     *       200:
+     *         description: Cadastro inserido com sucesso
+     *         content:
+     *           application/json:
+     *             example:
+     *               result:
+     *                 codigo: 123
+     *                 nomeSobrenome: John Doe
+     *                 nomeUsuario: johndoe
+     *                 email: johndoe@example.com
+     *                 senha: ********
+     *       400:
+     *         description: Campos não enviados
+     *         content:
+     *           application/json:
+     *             example:
+     *               error: Campos não enviados
+     *     parameters:
+     *       - in: body
+     *         name: body
+     *         required: true
+     *         description: Objeto de dados do cadastro
+     *         schema:
+     *           type: object
+     *           properties:
+     *             nomeSobrenome:
+     *               type: string
+     *             nomeUsuario:
+     *               type: string
+     *             email:
+     *               type: string
+     *             senha:
+     *               type: string
+     */
+    inserir: async (req, res) => {
+        let json = { error: '', result: {} };
 
         let nomeSobrenome = req.body.nomeSobrenome;
         let nomeUsuario = req.body.nomeUsuario;
         let email = req.body.email;
         let senha = req.body.senha;
 
-        if (nomeSobrenome && nomeUsuario && email && senha){
-            let cadastroCodigo = await cadastroServices.inserir(nomeSobrenome, nomeUsuario, email, senha);
-            json.result = {
-                codigo: cadastroCodigo,
-                nomeSobrenome,
-                nomeUsuario,
-                email,
-                senha
-            };
-        }else{
+        if (nomeSobrenome && nomeUsuario && email && senha) {
+            try {
+                let cadastroCodigo = await cadastroServices.inserir(nomeSobrenome, nomeUsuario, email, senha);
+                json.result = {
+                    codigo: cadastroCodigo,
+                    nomeSobrenome,
+                    nomeUsuario,
+                    email,
+                    senha: '********' // Evitar mostrar senhas no exemplo de resposta
+                };
+                res.json(json);
+            } catch (error) {
+                json.error = 'Erro ao inserir cadastro';
+                res.status(500).json(json);
+            }
+        } else {
             json.error = 'Campos não enviados';
+            res.status(400).json(json);
         }
-        res.json(json);
     },
-
-}
+};
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config({path:'variaveis.env'});
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const routes = require('./routes');
 
@@ -10,6 +12,21 @@ server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
 
 server.use('/api', routes);
+
+// Configurando Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: 'API de Cadastro',
+        description: 'API para gerenciamento de cadastros',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./controllers/*.js'], // Seus controladores ou rotas que contêm as anotações JSDoc
+  };
+  
+  const swaggerSpec = swaggerJsdoc(swaggerOptions);
+  server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 server.listen(process.env.PORT,()=>{
     console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
